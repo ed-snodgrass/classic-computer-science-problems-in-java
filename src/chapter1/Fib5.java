45c38fb4c379915e640d61ce26d2ace08a3486c3
ed.snodgrass <snodgrass.ed@gmail.com>
2021-01-23 14:56:46 -0600

    chapter 1, added tests for the fibonacci stream implementation. Also refactored the stream function to handle the limit based on th number passed in.

--- src/chapter1/Fib5.java
+++ src/chapter1/Fib5.java
@@ -5,17 +5,12 @@ import java.util.stream.IntStream;
 public class Fib5 {
     private int last = 0, next = 1;
 
-    public IntStream stream() {
+    public IntStream stream(int n) {
         return IntStream.generate(() -> {
             int oldLast = last;
             last = next;
             next = oldLast + next;
             return oldLast;
-        });
-    }
-
-    public static void main(String[] args) {
-        Fib5 fib5 = new Fib5();
-        fib5.stream().limit(41).forEachOrdered(System.out::println);
+        }).limit(n + 1);
     }
 }
---
+++ test/chapter1/Fib5Test.java
@@ -0,0 +1,45 @@
+package chapter1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class Fib5Test {
+
+
+    @Test
+    void fibonacciStreamTestWithZero() {
+        Fib5 fib5 = new Fib5();
+        assertThat(fib5.stream(0).max()).hasValue(0);
+    }
+
+    @Test
+    void fibonacciStreamTestWithOne() {
+        Fib5 fib5 = new Fib5();
+        assertThat(fib5.stream(1).max()).hasValue(1);
+    }
+
+    @Test
+    void fibonacciStreamTestWithTwo() {
+        Fib5 fib5 = new Fib5();
+        assertThat(fib5.stream(2).max()).hasValue(1);
+    }
+
+    @Test
+    void fibonacciStreamTestWithThree() {
+        Fib5 fib5 = new Fib5();
+        assertThat(fib5.stream(3).max()).hasValue(2);
+    }
+
+    @Test
+    void fibonacciStreamTestWithFour() {
+        Fib5 fib5 = new Fib5();
+        assertThat(fib5.stream(4).max()).hasValue(3);
+    }
+
+    @Test
+    void fibonacciStreamTestWithFive() {
+        Fib5 fib5 = new Fib5();
+        assertThat(fib5.stream(5).max()).hasValue(5);
+    }
+}
