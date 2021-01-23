c4a67127a8955f2cb7c72d0b84d20587fd966507
ed.snodgrass <snodgrass.ed@gmail.com>
2021-01-23 15:06:02 -0600

    chapter 1, added tests for the fibonacci iterative implementation.

--- src/chapter1/Fib4.java
+++ src/chapter1/Fib4.java
@@ -1,7 +1,7 @@
 package chapter1;
 
 public class Fib4 {
-    private static int fib4(int n) {
+    public int fib4(int n) {
         int last = 0, next = 1;
         for (int i = 0; i < n; i++) {
             int oldLast = last;
@@ -9,10 +9,5 @@ public class Fib4 {
             next = next + oldLast;
         }
         return last;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(fib4(20));
-        System.out.println(fib4(40));
     }
 }
---
+++ test/chapter1/Fib4Test.java
@@ -0,0 +1,57 @@
+package chapter1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Fib4Test {
+
+    @Test
+    void fibonacciTestWithZero() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(0)).isEqualTo(0);
+    }
+
+    @Test
+    void fibonacciTestWithOne() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(1)).isEqualTo(1);
+    }
+
+    @Test
+    void fibonacciTestWithTwo() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(2)).isEqualTo(1);
+    }
+
+    @Test
+    void fibonacciTestWithThree() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(3)).isEqualTo(2);
+    }
+
+    @Test
+    void fibonacciTestWithFour() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(4)).isEqualTo(3);
+    }
+
+    @Test
+    void fibonacciTestWithFive() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(5)).isEqualTo(5);
+    }
+
+    @Test
+    void fibonacciTestWithTwenty() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(20)).isEqualTo(6765);
+    }
+
+    @Test
+    void fibonacciTestWithForty() {
+        Fib4 fib4 = new Fib4();
+        assertThat(fib4.fib4(40)).isEqualTo(102334155);
+    }
+}
