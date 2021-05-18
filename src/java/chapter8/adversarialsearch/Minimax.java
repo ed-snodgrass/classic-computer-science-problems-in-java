cb7dc2ebf554b7686094cb6da4d33deae6d917a2
ed.snodgrass <snodgrass.ed@gmail.com>
2021-05-17 22:50:54 -0500

    8.3.3 Added alpha-beta pruning to minimax

--- src/java/chapter8/adversarialsearch/Minimax.java
+++ src/java/chapter8/adversarialsearch/Minimax.java
@@ -26,12 +26,41 @@ public class Minimax {
         double bestEval = Double.NEGATIVE_INFINITY;
         Move bestMove = null;
         for (Move move: board.getLegalMoves()) {
-            double result = minimax(board.move(move), false, board.getTurn(), maxDepth);
+            double result = alphabeta(board.move(move), false, board.getTurn(), maxDepth);
             if (result > bestEval) {
                 bestEval = result;
                 bestMove = move;
             }
         }
         return bestMove;
+    }
+
+    private static <Move> double alphabeta(Board<Move> board, boolean maximizing, Piece originalPlayer, int maxDepth) {
+        return alphabeta(board, maximizing, originalPlayer, maxDepth, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+
+    private static <Move> double alphabeta(Board<Move> board, boolean maximizing, Piece originalPlayer, int maxDepth, double alpha, double beta) {
+        //Base case - terminal position or maximum depth reached
+        if (board.isWin() || board.isDraw() || maxDepth == 0) {
+            return board.evaluate(originalPlayer);
+        }
+        // Recursive case - maximize your gains or minimize the opponent's
+        if (maximizing) {
+            for (Move m : board.getLegalMoves()) {
+                alpha = Math.max(alpha, alphabeta(board.move(m), false, originalPlayer, maxDepth - 1, alpha, beta));
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return alpha;
+        } else {
+            for (Move m : board.getLegalMoves()) {
+                beta = Math.min(beta, alphabeta(board.move(m), true, originalPlayer, maxDepth - 1, alpha, beta));
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return beta;
+        }
     }
 }
